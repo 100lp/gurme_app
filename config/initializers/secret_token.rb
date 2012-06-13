fe41795ecc6d7899a095e46a4377f840401832d0
Nikita Web <spb.gurme@gmail.com>
2012-06-13 13:35:42 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GurmeApp::Application.config.secret_token = 'f73cc05690e76d3e079dd980facb68fb6d5047f36ca098600d95d8e3ce4ed2110ae38523106e71418192e348bd01c1e681b94063b61ffbc5603389b8783a71f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GurmeApp::Application.config.session_store :cookie_store, :key => '_gurme_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GurmeApp::Application.config.session_store :active_record_store
