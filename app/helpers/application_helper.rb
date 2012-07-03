e70dbfc3a00b26db8f84ff1c354ac389cdc39c2a
Nikita Web <spb.gurme@gmail.com>
2012-07-03 13:31:23 +0400

    Initial commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 def menu_link(text, src)
-  link_to text, src, class: current_page?(src) and 'active'
+  link_to text, src, class: (current_page?(src) and 'active')
 end
 
 end
